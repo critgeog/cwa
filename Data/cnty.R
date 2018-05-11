349250d77633182a01f2af0ca3b0e719063505e1
Taylor Hafley <taylor.hafley.uga@gmail.com>
2018-05-11 14:07:15 -0400

    new_data

---
+++ Data/cnty.R
@@ -0,0 +1,104 @@
+
+
+"McLennan","Falls","Bell","Coryell","Limestone","Hill","Bosque","Freestone",
+"Hamilton","Leon","Mills","Cameron","Hidalgo","Willacy","Jimm Hogg","Kennedy","Starr",
+"Zapata","Brooks","Duval","Chesterfield","Amelia","Caroline"
+"Charles City","Dinwiddie","Goochland","Hanover","Henrico","King William",
+"New Kent","Powhatan","Prince George","Sussex","Brunswick","Charlotte","Greensville",
+"Cumberland","Nottoway","Lunenburg","Mecklenberg","Prince Edward","Southampton","Surry",
+"James City","Matthews","York","Currituck","Gates",
+"Gloucester","Chesapeake",
+Hampton
+Newport News
+Norfolk
+Portsmouth
+Suffolk
+Virginia Beach
+Williamsburg
+Isle of Wight
+Spotsylvania
+Fredricksburg City
+Louisa
+Orange
+Fluvanna
+Albemarle
+Buckingham
+King and Queen
+Lancaster
+Middlesex
+Northumberland
+Richmond
+Westmoreland
+Essex
+Salt Lake
+Tooele
+Davis
+Morgan
+Weber
+Box Elder
+Piute
+Summit
+Wasatch
+Rich
+Juab
+Utah
+Dona Ana
+Luna
+Sierra
+Grant
+Hidalgo
+Otero
+Chaves
+Eddy
+Lincoln
+Clark
+Fayette
+Jessamine
+Scott
+Woodford
+Bourbon
+Madison
+Menifee
+Mercer
+Montgomery
+Nicholas
+Owen
+Powell
+Robertson
+Rockcastle
+Rowan
+Washington
+Anderson
+Bath
+Boyle
+Estill
+Fleming
+Franklin
+Garrard
+Harrison
+Lincoln
+Laurel
+Whitley
+Marion
+McCreary
+Pulaski
+Taylor
+Wayne
+Whitley
+Marion
+Casey
+Green
+Knox
+Bell
+Breathitt
+Clay
+Harlan
+Jackson
+Knott
+Lee
+Leslie
+Letcher
+Morgan
+Owsley
+Perry
+Wolfe
